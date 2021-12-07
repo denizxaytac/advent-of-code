da6961a968551361a6dfc91a64a76d1a6fddfeb4
dnz <denizaytach317@gmail.com>
2021-12-07 09:41:47 +0300

    Solved the 7th day :tada:

---
+++ day7/part1.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+def get_lowest(num):
+    return abs(common[0] - num)
+
+with open('input.txt', 'r') as f:
+    horizontal_positions = f.read().split(',')
+    horizontal_positions = [int(i) for i in horizontal_positions]
+    c = Counter(horizontal_positions)
+    mean_ = c.most_common()
+    lowest_sum = float('inf')
+    for common in mean_:
+        x = sum(map(get_lowest, horizontal_positions))
+        lowest_sum = min(x, lowest_sum)
+print(lowest_sum)
