5ced523d3b7f9ef06c4ae70355e7b587e4dfe371
dnz <denizaytach317@gmail.com>
2021-12-31 11:05:53 +0300

    Solved 2015, day04 :tada:

---
+++ 2015/day04/part2.py
@@ -0,0 +1,12 @@
+import hashlib
+def find_min(key):
+    for i in range(100000000):
+        txt = key + str(i)
+        result =  hashlib.md5(txt.encode())
+        if result.hexdigest()[:6] == "000000":
+            return i
+    return None
+
+
+if __name__ == "__main__":
+    print(find_min("iwrupvqb"))
