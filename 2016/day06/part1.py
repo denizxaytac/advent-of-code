7cd97e4979715b12f18a99f89ea78b8d27c504fa
deniz aytac <denizaytach317@gmail.com>
2022-07-15 21:29:49 +0300

    Solved 2016 day 06 :tada:

---
+++ 2016/day06/part1.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+def solution(fname):
+    with open(fname, 'r') as f:
+        content = f.read().strip().split('\n')
+    answer = ""
+    idx_col = 0
+    for _ in range(len(content[0])):
+        column_str = ""
+        for idx_row in range(len(content)):
+            column_str += content[idx_row][idx_col]
+        idx_col += 1
+        column_list = Counter(column_str)
+        answer += column_list.most_common(1)[0][0]
+    return answer
+if __name__ == "__main__":
+    print(solution('input.txt'))
