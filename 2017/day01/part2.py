a1a076f837c44bdbd4e000134b9edb0356aaeaf0
deniz <denizaytach317@gmail.com>
2022-10-17 20:47:55 +0300

    Solved 2017 day 01 :sparkles:

---
+++ 2017/day01/part2.py
@@ -0,0 +1,15 @@
+
+def solution(fname):
+    with open(fname, 'r') as f:
+        content = f.read().strip().split('\n')[0]
+    total_sum = 0
+    length = len(content)
+    for idx, char in enumerate(content):
+        next_char = content[int(idx + (length / 2)) % length]
+        if char == next_char:
+            total_sum += int(next_char)
+
+    return total_sum
+
+if __name__ == "__main__":
+    print(solution('input.txt'))
