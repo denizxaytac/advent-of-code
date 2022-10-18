8ab8a6587822b0aaee2ca55b8ec6a01e95b79202
deniz <denizaytach317@gmail.com>
2022-10-18 14:19:03 +0300

    Solved 2017 day 04 :tada:

---
+++ 2017/day04/part1.py
@@ -0,0 +1,16 @@
+def solution(fname):
+    with open(fname, 'r') as f:
+        content = f.read().strip().split('\n')
+    valid_pass = 0
+    for line in content:
+        phrases = line.split(' ')
+        valid = True
+        for phrase in phrases:
+            if phrases.count(phrase) > 1:
+                valid = False
+        if valid:
+            valid_pass += 1
+    return valid_pass
+
+if __name__ == "__main__":
+    print(solution('input.txt'))
