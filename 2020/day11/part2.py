d2186bfa576e901b133e74839c6f9ec66080944e
deniz aytac <denizaytach317@gmail.com>
2022-11-28 22:04:47 +0300

    Solved 2020 day 11 :tada:

---
+++ 2020/day11/part2.py
@@ -0,0 +1,53 @@
+from copy import deepcopy
+
+# # -> occupied | L -> empty | . -> floor
+def change_state(grid):
+    new_grid = list()
+    for idx_r, row in enumerate(grid):
+        new_row = list()
+        for idx_c, col in enumerate(row):
+            neighbours = {"empty": 0, "floor": 0, "occupied": 0}
+            for r_dir in range(-1, 2, 1):
+                for c_dir in range(-1, 2, 1):
+                    rr = r_dir
+                    cc = c_dir
+                    while True:
+                        if rr == 0 and cc == 0:
+                            break
+                        if idx_r + rr < 0 or idx_r + rr > len(grid) - 1 or idx_c + cc < 0 or idx_c + cc > len(grid[0]) - 1:
+                            break
+                        if grid[idx_r + rr][idx_c + cc] != ".":
+                            if grid[idx_r + rr][idx_c + cc] == ".":
+                                neighbours["floor"] += 1
+                            elif grid[idx_r + rr][idx_c + cc] == "#":
+                                neighbours["occupied"] += 1
+                                break
+                            elif grid[idx_r + rr][idx_c + cc] == "L":
+                                neighbours["empty"] += 1
+                                break
+                        cc += c_dir
+                        rr += r_dir
+            if col == ".":
+                new_row.append(".")
+            elif col == "L" and neighbours["occupied"] == 0:
+                new_row.append("#")
+            elif col == "#" and neighbours["occupied"] >= 5:
+                new_row.append("L")
+            else:
+                new_row.append(grid[idx_r][idx_c])
+        new_grid.append(new_row)
+    return new_grid, (new_grid != grid)
+
+def solution(fname):
+    with open(fname, 'r') as f:
+        content = f.read().strip().split('\n')
+    grid = list()
+    for line in content:
+        grid.append([char for char in line])
+    change = True
+    while change:
+        grid, change = change_state(grid)
+    return sum(sum([[1 if val == '#' else 0 for val in row] for row in grid], []))
+
+if __name__ == "__main__":
+    print(solution('input.txt')) 
