652d5e6441fec2c23fead456950973ac946a8a3e
deniz aytac <denizaytach317@gmail.com>
2022-12-06 17:28:09 +0300

    Solved 2022 day 6 :tada:

---
+++ 2022/day06/part1.py
@@ -0,0 +1,13 @@
+
+def solution(fname, length):
+    with open(fname, 'r') as f:
+        content = f.read().strip().split('\n')[0]
+    for end, char in enumerate(content):
+        start = end - length
+        if start < 0:
+            start = 0
+        last_four_chars = content[start:end]
+        if len(set(last_four_chars)) == length:
+            return end
+if __name__ == "__main__":
+    print(solution('input.txt', 4)) 
